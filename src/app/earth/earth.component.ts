efcf7624e98d9ffd828db6e5537893877a0bb4b5
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-09 22:37:30 +0530

    done

--- src/app/earth/earth.component.ts
+++ src/app/earth/earth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-earth',
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./earth.component.css']
 })
 export class EarthComponent implements OnInit {
+  constructor(private dataService: DataService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/venus/venus.component.ts
+++ src/app/venus/venus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-venus',
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./venus.component.css']
 })
 export class VenusComponent implements OnInit {
+  constructor(private dataService: DataService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
