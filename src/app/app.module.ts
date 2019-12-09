91134a3f5eaf027186605b42849d7d85aec4a0e3
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-09 22:11:14 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MercuryComponent } from './mercury/mercury.component';
+import { VenusComponent } from './venus/venus.component';
+import { EarthComponent } from './earth/earth.component';
+import { MarsComponent } from './mars/mars.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MercuryComponent, VenusComponent, EarthComponent, MarsComponent, NavbarComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
